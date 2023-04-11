c68d11914537d0d2922831c19bc3c025c44867cf
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-04-11 16:48:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ window.addEventListener('load', function() {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  var cursor = document.createElement("img");
+  cursor.src = "https://www.transparentpng.com/thumb/mouse-cursor/lHbsTB-download-mouse-cursor-png.png";
+  cursor.style.width = "50px";
+  cursor.style.height = "50px";
+  cursor.style.position = "fixed";
+  cursor.style.pointerEvents = "none";
+  document.body.appendChild(cursor);
+
   document.addEventListener("mousemove", function(e) {
     cursor.style.left = (e.clientX - 25) + "px";
     cursor.style.top = (e.clientY - 25) + "px";
